ca27feaa8d4b8d5b3fc05235705eafdec172c20a
aidingdongg <gakramaqil@gmail.com>
2022-10-29 15:21:58 +0800

    added contracts OrderbookFactory and Orderbook

--- frontend/contracts/Dex.sol
+++ frontend/contracts/Dex.sol
@@ -2,21 +2,284 @@ pragma solidity ^0.8.17;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
-    address public admin;
+/* OrderbookFactory
+   ===========================
+   Creates a new Orderbook between 2 supported tokens
+*/
+contract OrderbookFactory {
+    //Existing Orderbooks
+    mapping(bytes32 => address) public orderbooks;
+    event newPair(address indexed token1, address indexed token2);
+    function addPair(address token1, address token2) external {
+        require(token1 != token2, "Token must be different!");
 
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
-        admin = msg.sender;
-        _mint(msg.sender, 1000);
+        bytes32 orderbookID = keccak256(abi.encodePacked(token1, token2)); // Compute orderbookID from  the hash of the ordered address
+        require(orderbooks[orderbookID] == address(0), "Token pair already exists"); 
+
+        orderbooks[orderbookID] = adress(new Orderbook(token1, token2)); // Instantiate new Orderbook contract
+        emit newPair(token1, token2);
+    }
+}
+
+contract Orderbook{
+    string name; //token2/token1
+    IERC20 token1;
+    IERC20 token2;
+    
+    struct Order{
+        uint256 price;
+        uint256 quantity;
+        uint256 unitPrice;
+        uint256 date;
     }
 
-    function mint(address _to, uint256 _amount) external {
-        require(msg.sender == admin, "Only admin can mint");
-        _mint(_to, _amount);
+    mapping(address => Order) buyOrders; //Maps Buy Orders
+    mapping(address => address) nextBuy; 
+    uint256 public buyCount;
+
+    mapping(address => Order) sellOrders; //Maps Sell Orders
+    mapping(address => address) nextSell;
+    uint256 public sellCount;
+
+    address constant BUFFER = address(1);
+
+    event CancelBuyOrder(address indexed buyer);
+    event CancelSellOrder(address indexed seller);
+
+    event BuyOrderPlaced(
+        uint256 indexed unitPrice,
+        uint256 price,
+        uint256 quantity,
+        address indexed buyer
+    );
+    event SellOrderPlaced(
+        uint256 indexed unitPice,
+        uint256 price,
+        uint256 quantity
+        address indexed seller
+    );
+
+    /* Constructor
+       =========================================
+    */
+    constructor(address token1, address token2){
+        nextBuy[BUFFER] = BUFFER;
+        nextSell[BUFFER] = BUFFER;
+    }
+
+    function _verifyIndexBuy( 
+        address prev,
+        uint256 unitPrice,
+        address next
+    ) internal view returns (bool) {
+        return ((prev == BUFFER || unitPrice <= buyOrders[prev].unitPrice) && 
+            (next == BUFFER || unitPrice > buyOrders[next].unitPrice));
+    }
+    function _verifyIndexSell(
+        address prev,
+        uint256 unitPrice,
+        address next
+    ) internal view returns (bool) {
+        return ((prev == BUFFER || unitPrice >= sellOrders[prev].unitPrice) &&
+            (next == BUFFER || unitPrice < sellOrders[next].unitPrice));
+    }
+    
+
+    function _findPrevBuy(uint256 unitPrice) internal view returns (address) {
+        address prev = BUFFER;
+        while (true) {
+            if (_verifyIndexBuy(prev, unitPrice, nextBuy[prev])) {
+                return prev;
+            }
+            prev = nextBuy[prev];
+        }
+    }
+    function _verifyIndexSell(
+        address prev,
+        uint256 unitPrice,
+        address next
+    ) internal view returns (bool) {
+        return ((prev == BUFFER || unitPrice >= sellOrders[prev].unitPrice) &&
+            (next == BUFFER || unitPrice < sellOrders[next].unitPrice));
+    }
+
+    function _getPrevious(address target) internal view returns (address) {
+        address current = BUFFER;
+        while (nextBuy[current] != BUFFER) {
+            if (nextBuy[current] == target) {
+                return current;
+            }
+            current = nextBuy[current];
+        }
+    }
+    
+    function placeBuy(uint256 _price, uint256 _quantity) external { // Places a buy order and locks associated collateral
+        require( 
+            buyOrders[msg.sender].date == 0, // Only one buy order per address
+            "First delete existing buy order"
+        );
+        require(
+            _price != 0 && _quantity != 0,
+            "Must have nonzero pice and quantity"
+        );
+
+        buyOrders[msg.sender] = Order(_price, _quantity, block.timestamp, _price/_quantity); // Create a new order in the buy order mapping for msg.sender
+
+        // Add msg.sender into the appropriate position in the ordering mapping. This is similar to linked list insertion
+        address prev = _findPrevBuy(_price/_quantity);
+        address temp = nextBuy[prev];
+        nextBuy[prev] = msg.sender;
+        nextBuy[msg.sender] = temp;
+
+        buyCount++; // Increment the overall buy count
+        token1.transferFrom(msg.sender, address(this), _price); // Transfer the buy order price of token1 from the buyer to the orderbook contract. This locks the associated collateral
+        emit BuyOrderPlaced(_price/_quantity,_price, _quantity, msg.sender);  // Emit buy order placed event
+    }
+
+    function cancelBuy() external { // Cancels the buy order associated with msg.sender if it exists
+        require(
+            buyOrders[msg.sender].date != 0,
+            "Buy order must already exist"
+        );
+        uint256 quantity = buyOrders[msg.sender].quantity; // Store quantity of buy order to refund msg.sender with correct amount
+        address prev = _getPrevious(msg.sender); // Find the previous address of the msg.sender in the ordering mapping
+        nextBuy[prev] = nextBuy[msg.sender]; // Delete msg.sender from ordering mapping. Similar to linked list deletion
+
+        // Delete buy order from buy order mapping and ordering mapping
+        delete nextBuy[msg.sender];
+        delete buyOrders[msg.sender];
+        buyCount--; // Decrement the buy count
+        token1.transfer(msg.sender, quantity); // Unlock associated collateral and send it back to msg.sender
+        emit CancelBuyOrder(msg.sender); // Emit a cancel buy order event
+    }
+
+    function placeSell(uint256 _price, uint256 _quantity) external { // Places a sell order and locks associated collateral
+        require(
+            sellOrders[msg.sender].date == 0, // Only one sell order per address
+            "First delete existing sell order"
+        );
+        require(
+            _price != 0 && _quantity != 0,
+            "Must have nonzero pice and quantity"
+        );
+
+        sellOrders[msg.sender] = Order(_price, _quantity, block.timestamp,_price/_quantity); // Create a new order in the sell order mapping for msg.sender
+
+        // Add msg.sender into the appropriate position in the ordering mapping. This is similar to linked list insertion
+        address prev = _findPrevSell(_price/_quantity);
+        address temp = nextSell[prev];
+        nextSell[prev] = msg.sender;
+        nextSell[msg.sender] = temp;
+        sellCount++; // Increment the sell count
+        token2.transferFrom(msg.sender, address(this), _quantity); //Transfer the sell order quantity of token2 from the seller to the orderbook contract. This locks the associated collateral
+        emit SellOrderPlaced(_price/_quantity,_price, _quantity, msg.sender); // Emit a sell order placed event
+    }
+
+    // Cancels the sell order associated with msg.sender if it exists
+    function cancelSell() external {
+        require(
+            sellOrders[msg.sender].date != 0,
+            "Sell order must already exist"
+        );
+
+        
+        uint256 quantity = sellOrders[msg.sender].quantity; // Store quantity of sell order to refund msg.sender with correct amount
+        address prev = _getPrevious(msg.sender); // Find the previous address of the msg.sender in the ordering mapping
+        nextSell[prev] = nextSell[msg.sender]; // Delete msg.sender from ordering mapping. Similar to linked list deletion
+
+        // Delete sell order from sell order mapping and ordering mapping
+        delete nextSell[msg.sender];
+        delete sellOrders[msg.sender];
+
+        
+        sellCount--; // Decrement sell count
+        token2.transferFrom(address(this), msg.sender, quantity); // Unlock associated collateral and send it back to msg.sender
+        emit CancelSellOrder(msg.sender); // Emit a cencel sell order event
+    }
+
+    /* Returns the buy side of the orderbook in three separate arrays. The first
+     * array contains all the addresses with active buy orders, and the second
+     * and third arrays contain the associated prices and quantities of these
+     * buy orders respectively. Arrays are returned in descending order
+     */
+    function getBuySide()
+        external
+        view
+        returns (
+            address[] memory,
+            uint256[] memory,
+            uint256[] memory,
+            uint256[] memory
+        )
+    {
+        // Instantiate three arrays equal in length to the total buy count
+        address[] memory addressTemp = new address[](buyCount);
+        uint256[] memory unitPriceTemp = new uint256[](buyCount);
+        uint256[] memory priceTemp = new uint256[](buyCount);
+        uint256[] memory quantityTemp = new uint256[](buyCount);
+
+        // Set current address equal to the first buy order address
+        address current = nextBuy[BUFFER];
+
+        // Iterate through each array and store the corresponding values
+        for (uint256 i = 0; i < addressTemp.length; i++) {
+            addressTemp[i] = current;
+            Order storage order = buyOrders[current];
+
+            unitPriceTemp[i] = order.unitPrice;
+            priceTemp[i] = order.price;
+            quantityTemp[i] = order.quantity;
+
+            current = nextBuy[current];
+        }
+
+        // Return the three arrays
+        return (addressTemp, unitPriceTemp, priceTemp, quantityTemp);
+    }
+
+    /* Returns the sell side of the orderbook in three separate arrays. The first
+     * array contains all the addresses with active sell orders, and the second
+     * and third arrays contain the associated prices and quantities of these
+     * sell orders respectively. Arrays are returned in ascending order
+     */
+    function getSellSide()
+        external
+        view
+        returns (
+            address[] memory,
+            uint256[] memory,
+            uint256[] memory,
+            uint256[] memory
+        )
+    {
+        // Instantiate three arrays equal in length to the total sell count
+        address[] memory addressTemp = new address[](sellCount);
+        uint256[] memory unitPriceTemp = new uint256[](sellCount);
+        uint256[] memory priceTemp = new uint256[](sellCount);
+        uint256[] memory quantityTemp = new uint256[](sellCount);
+
+        // Set current address equal to the first sell order address
+        address current = nextSell[BUFFER];
+
+        // Iterate through each array and store the corresponding values
+        for (uint256 i = 0; i < addressTemp.length; i++) {
+            addressTemp[i] = current;
+            Order storage order = sellOrders[current];
+
+            unitPriceTemp[i] = order.unitPrice;
+            priceTemp[i] = order.price;
+            quantityTemp[i] = order.quantity;
+
+            current = nextSell[current];
+        }
+
+        // Return the three arrays
+        return (addressTemp, unitPriceTemp, priceTemp, quantityTemp);
     }
 }
 
 contract Dex {
+  
     mapping (address => ERC20) private availableTokens;
     address[] private tokenAddresses;
     uint256 private numTokens;
@@ -83,3 +346,16 @@ contract Dex {
     }
 }
 
+contract Token is ERC20 {
+    address public admin;
+
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        admin = msg.sender;
+        _mint(msg.sender, 1000);
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        require(msg.sender == admin, "Only admin can mint");
+        _mint(_to, _amount);
+    }
+}
